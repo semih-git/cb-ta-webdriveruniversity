5a4f9cdbcab607060bb13031da366e6c30cbfb83
semih-git <cemcan34355@gmail.com>
2021-01-01 23:10:02 +0100

    read and write data to excel using apache poi

--- src/main/java/org/cb/ta/WDUDataTablesButtonStatesPage.java
+++ src/main/java/org/cb/ta/WDUDataTablesButtonStatesPage.java
@@ -4,18 +4,27 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
+import java.util.List;
+
 public class WDUDataTablesButtonStatesPage {
     private WebDriver driver;
     private final WebElement headerElement;
+    private final List<WebElement> table1Rows;
 
     public WDUDataTablesButtonStatesPage(WebDriver driver) {
         this.driver = driver;
         this.driver.get("http://www.webdriveruniversity.com/Data-Table/index.html");
 
+
         headerElement = driver.findElement(By.id("main-header"));
+        table1Rows = driver.findElements(By.xpath("//table[@id='t01']/tbody/tr"));
     }
 
     public WebElement getHeaderElement() {
         return headerElement;
     }
+    public List<WebElement> getTable1Rows() {
+        return table1Rows;
+    }
+
 }
--- src/test/java/org/cb/ta/WDUDataTablesButtonStatesPageTest.java
+++ src/test/java/org/cb/ta/WDUDataTablesButtonStatesPageTest.java
@@ -1,14 +1,21 @@
 package org.cb.ta;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.testng.Assert.assertNotNull;
+import static org.testng.AssertJUnit.assertEquals;
 
 public class WDUDataTablesButtonStatesPageTest extends BaseTest {
 
     private WDUDataTablesButtonStatesPage wduDataTablesButtonStatesPage;
+    private WriteDataToExcel writeDataToExcel = new WriteDataToExcel();
 
     public WDUDataTablesButtonStatesPageTest() {
         wduDataTablesButtonStatesPage = new WDUDataTablesButtonStatesPage(driver);
@@ -16,6 +23,44 @@ public class WDUDataTablesButtonStatesPageTest extends BaseTest {
 
     @Test
     public void simple() {
-        assertNotNull(wduDataTablesButtonStatesPage.getHeaderElement());
+        String textOfHeader =  wduDataTablesButtonStatesPage.getHeaderElement().getText();
+//        System.out.println(textOfHeader);
+        assertEquals("Data, Tables & Button States", textOfHeader);
+    }
+
+    @Test
+    public void testTable1() throws IOException {
+        List<WebElement> rows = wduDataTablesButtonStatesPage.getTable1Rows();
+//        String[] dataToWrite = new String[4];
+        List<String[]> dataList = new ArrayList<>();
+        for (WebElement row : rows) {
+            String[] splittedRow = row.getText().split(" ");
+//            ["Firstname", "Lastname", "Age"]
+            dataList.add(splittedRow);
+//            System.out.println(row.getText());
+        }
+
+        // [["Firstname", "Lastname", "Age"],["John", "Smith", "45"],["Jemma", "Jackson", "94"],["Michael", "Doe", "20"]]
+        writeDataToExcel.writeExcel("src/test/java/org/cb/ta/resources/", "test01.xls", "Sayfa1", dataList );
+
+        /*
+         * Firstname lastname Age
+         * sally     abc       20
+         * john      xyz       30
+         *
+         * websitesinden cekilen veriler ile text boxlara yazilip table olmasini bekledigimiz verileri karsilastiralim
+         * bunun icin
+         * 1- textboxlara input gir
+         * 2- submitlemeye calis
+         * 3- tekrar veri cek
+         * 4- son satirlari karsilastir ve
+         * 5- mesaj don.
+         *
+         *
+         * */
+
+        // writeDataToExcel.writeExcel("src/test/java/org/cb/ta/resources/", "test01.xls", "Table01",  );
+
+
     }
 }
