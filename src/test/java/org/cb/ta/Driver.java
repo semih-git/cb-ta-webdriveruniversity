2fc612e33ae9a7d470753881e3868c7ddc342d9b
semih-git <cemcan34355@gmail.com>
2021-01-01 22:49:48 +0100

    I modified drive file.

--- src/test/java/org/cb/ta/Driver.java
+++ src/test/java/org/cb/ta/Driver.java
@@ -6,8 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public abstract class Driver {
     public static WebDriver getDriver() {
         System.setProperty("webdriver.chrome.driver","webdriver\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
-        return driver;
+        //driver.manage().window().maximize();
+        return new ChromeDriver();
     }
 }
